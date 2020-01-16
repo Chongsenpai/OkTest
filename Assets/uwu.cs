b2ede51d3857f394c3f00a7829b342de8775473e
SimonBruun <bsb.bruun@hotmail.com>
2020-01-16 13:32:56 +0100

    uwu

--- Assets/uwu.cs
+++ Assets/uwu.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class uwu : MonoBehaviour
 {
     [SerializeField] GameObject SpawnObject;
+    [SerializeField] GameObject Target;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject TempSpawn = Instantiate(SpawnObject, Target.transform.position, Target.transform.rotation);
+        Destroy(TempSpawn, 3f);
+    }
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
