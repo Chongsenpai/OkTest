19aa3afb2e615572c269b66f4ecd733d48979957
Chongsenpai <37104800+Chongsenpai@users.noreply.github.com>
2020-01-16 12:28:01 +0100

    sad

---
+++ Assets/uwu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uwu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if uwu.c# exists =
+              delete system32
+        else:
+            still good game
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        repeat
+    }
+}
